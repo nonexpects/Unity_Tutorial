b53ad315584d90b61ddb695617acdb6a42ab608e
orionx6x <orionx6x@gmail.com>
2020-05-24 04:32:14 +0900

    part 10 - 파티클

---
+++ Unity_Tutorial/Assets/Scenes/Scripts/Part10_Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part10_Test : MonoBehaviour
+{
+    ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // play on awake 체크 안되어있으면 우리가 플레이를 해줘야함
+        ps.Play();
+        //입자가 숫자만큼 뿜어져나옴(Burst)
+        ps.Emit(300);
+    }
+}
