3e033e384aa411289dd0be8e6ae867a77887b90c
orionx6x <orionx6x@gmail.com>
2020-05-24 06:19:49 +0900

    part 16 - 오디오 소스

---
+++ Unity_Tutorial/Assets/Scenes/Scripts/Part16_Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part16_Test : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySE()
+    {
+        //int형이라 0~3까지만 포함됨
+        int _temp = Random.Range(0, 4);
+
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+    }
+}
