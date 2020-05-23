b789983e344e871a8454e948054cd978ee9f33b3
orionx6x <orionx6x@gmail.com>
2020-05-24 05:16:45 +0900

    part 13 - 인풋 필드 UI

---
+++ Unity_Tutorial/Assets/Scenes/Scripts/Part13_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part13_Test : MonoBehaviour
+{
+    [SerializeField] private Text txt_Money;
+    [SerializeField] private InputField inputTxt_Money;
+
+    private int currentMoney;
+
+    public void Input()
+    {
+        currentMoney += int.Parse(inputTxt_Money.text);
+
+        txt_Money.text = currentMoney.ToString();
+    }
+
+    public void Output()
+    {
+        currentMoney -= int.Parse(inputTxt_Money.text);
+
+        txt_Money.text = currentMoney.ToString();
+    }
+}
